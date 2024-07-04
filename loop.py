e0f5f7cb8c8961ca0444cd3fa8892b0328485df2
Subba Kona <subba.kona@gmail.com>
2024-07-04 15:20:02 +0530

    first commit from pycharm

---
+++ loop.py
@@ -0,0 +1,13 @@
+def myforloop() :
+    for i in range(11):
+        print( i," I am repeating 10 times in for")
+
+
+def mywhileloop() :
+    i=1
+    while i<11:
+        print( i ,  "I am repeating 10 times in while")
+        i+=1
+
+myforloop()
+mywhileloop()
